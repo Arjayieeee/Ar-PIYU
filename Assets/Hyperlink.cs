b4cea2cbb87e85ef9b918d7625e4409382389ea5
Arjayieeee <federioarjay16@gmail.com>
2023-01-12 13:27:46 +0800

    go

---
+++ Assets/Hyperlink.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hyperlink : MonoBehaviour
+{
+    public void ccs() {
+        Application.OpenURL("https://www.facebook.com/CCS.LSPU.SPCC");
+
+     } 
+
+     public void ssc() {
+        Application.OpenURL("https://www.facebook.com/lspuspccssc");
+
+     } 
+
+      public void ictso() {
+        Application.OpenURL("https://www.facebook.com/LSPUICTS");
+
+     } 
+}
